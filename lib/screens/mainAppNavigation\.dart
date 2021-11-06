219decbfc990c8144b56b8f8f44044b0cf157749
anupraj varma <anupraj1854@gmail.com>
2021-11-06 15:36:45 +0530

    swipable page

--- lib/screens/mainAppNavigation\.dart
+++ lib/screens/mainAppNavigation\.dart
@@ -14,47 +14,61 @@ class MainAppNavigation extends StatefulWidget {
 
 /// This is the private State class that goes with MyStatefulWidget.
 class _MainAppNavigationState extends State<MainAppNavigation> {
+  PageController _pageController = PageController(
+    initialPage: 0,
+  );
+
   int currentIndex = 0;
-  final Screen = [
-    HomeScreen(),
-    LikesScreen(),
-    HistoryScreen(),
-  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Screen[currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: AppColorSchemes.blue1,
-        selectedItemColor: AppColorSchemes.blue4,
-        unselectedItemColor: AppColorSchemes.blue3,
-        iconSize: 35,
-        selectedFontSize: 15,
-        items: [
-          BottomNavigationBarItem(
-            icon: currentIndex == 0
-                ? Icon(Icons.home)
-                : Icon(Icons.home_outlined),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: currentIndex == 1
-                ? Icon(Icons.favorite)
-                : Icon(Icons.favorite_outline),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: currentIndex == 2
-                ? Icon(Icons.history)
-                : Icon(Icons.history_outlined),
-            label: '',
-          ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (value) {
+          setState(() {
+            currentIndex = value;
+          });
+        },
+        children: [
+          HomeScreen(),
+          LikesScreen(),
+          HistoryScreen(),
         ],
-        currentIndex: currentIndex,
-        onTap: (index) => setState(() => currentIndex = index),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: AppColorSchemes.blue1,
+          selectedItemColor: AppColorSchemes.blue4,
+          unselectedItemColor: AppColorSchemes.blue3,
+          iconSize: 35,
+          selectedFontSize: 15,
+          items: [
+            BottomNavigationBarItem(
+              icon: currentIndex == 0
+                  ? Icon(Icons.home)
+                  : Icon(Icons.home_outlined),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: currentIndex == 1
+                  ? Icon(Icons.favorite)
+                  : Icon(Icons.favorite_outline),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: currentIndex == 2
+                  ? Icon(Icons.history)
+                  : Icon(Icons.history_outlined),
+              label: '',
+            ),
+          ],
+          currentIndex: currentIndex,
+          onTap: (index) {
+            currentIndex = index;
+            _pageController.animateToPage(index,
+                duration: Duration(milliseconds: 100), curve: Curves.linear);
+          }),
     );
   }
 }
