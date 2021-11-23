9de8df9cc5435d77dd5f148a15703b7d1940405e
anuprajvarma <anupraj1854@gmail.com>
2021-11-23 20:16:33 +0530

    adding textfield and button feature

--- lib/screens/newhomeScreen.dart
+++ lib/screens/newhomeScreen.dart
@@ -3,11 +3,14 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'package:http/http.dart' as http;
 import 'package:lyrical/components/Card.dart';
+import 'package:lyrical/components/myTextfield_for_artist.dart';
 
 import 'package:lyrical/constant/colorSchemes.dart';
+import 'package:lyrical/firebase/addHistory.dart';
 
 import 'package:lyrical/screens/welcomeScreen.dart';
 
@@ -99,7 +102,14 @@ class _HomeScreenState1 extends State<HomeScreen1> {
             ],
           ),
           body: Container(
-            child: Card(
+            width: 400,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(50),
+                    topLeft: Radius.circular(50)),
+                color: AppColorSchemes.blue1),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
               child: FutureBuilder(
                   future: getUserdata(),
                   builder: (context, AsyncSnapshot snapshot) {
@@ -108,8 +118,47 @@ class _HomeScreenState1 extends State<HomeScreen1> {
                         child: Column(
                           children: [
                             SizedBox(
-                              height: 25,
+                              height: 40,
                             ),
+                            MyTextField_artist(
+                              hintString: 'Search any lyrics or title',
+                              onChanged: (val) {
+                                artist = val;
+                              },
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                addHistory(artist, title);
+                              },
+                              child: Container(
+                                width: 180,
+                                height: 55,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: AppColorSchemes.blue4,
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(
+                                      FontAwesomeIcons.music,
+                                      size: 20,
+                                      color: AppColorSchemes.blue1,
+                                    ),
+                                    SizedBox(width: 10),
+                                    Text(
+                                      'Get Lyrics',
+                                      style: TextStyle(
+                                          color: AppColorSchemes.blue1,
+                                          fontSize: 15,
+                                          fontFamily: 'Poppins',
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 10),
                             Expanded(
                               child: GridView.count(
                                 crossAxisCount: 2,
@@ -122,7 +171,6 @@ class _HomeScreenState1 extends State<HomeScreen1> {
                           ],
                         ),
                       );
-                      ;
                     } else {
                       return Center(
                           child: Container(child: CircularProgressIndicator()));
